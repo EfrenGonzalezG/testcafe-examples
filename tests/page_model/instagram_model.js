52779c57f6322a9873947f58db38db4175ad8d84
EfrenGonzalezG <45051060+EfrenGonzalezG@users.noreply.github.com>
2020-03-29 15:48:43 -0600

    Update instagram_model.js

--- tests/page_model/instagram_model.js
+++ tests/page_model/instagram_model.js
@@ -6,6 +6,7 @@ export default class InstagramPage {
         this.inputPassword = Selector('input').withAttribute('name','password');
         this.buttonLogin = Selector('button').withAttribute('class','sqdOP  L3NKy   y3zKF     ');
         this.inputSearch = Selector('input').withAttribute('class','XTCLo x3qfX ');
+        this.aMyProfile = Selector('a').withAttribute('href','/dummyaccount411/');
         this.aProfile = Selector('a').withAttribute('href','/luiscormor/');
         this.buttonFollow = Selector('button').withAttribute('class','BY3EC  sqdOP  L3NKy   y3zKF     ');
         this.buttonUnfollow = Selector('button').withAttribute('class','BY3EC  sqdOP  L3NKy    _8A5w5    ');
